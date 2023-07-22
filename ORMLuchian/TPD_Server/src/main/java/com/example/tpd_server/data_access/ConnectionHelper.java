7b58d862445c84ccce21177dea2ae1f857056f91
LuchianMihai <luchiann.mihai@gmail.com>
2023-07-22 21:51:42 +0300

    v3

--- ORMLuchian/TPD_Server/src/main/java/com/example/tpd_server/data_access/ConnectionHelper.java
+++ ORMLuchian/TPD_Server/src/main/java/com/example/tpd_server/data_access/ConnectionHelper.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public final class ConnectionHelper {
-    private static final String url = "jdbc:postgresql:// 34.116.236.127:5432/postgres?socketFactory=com.google.cloud.sql.postgres.SocketFactory&cloudSqlInstance= astute-heaven-393613:europe-central2:postgres&user=postgres&password=postgres";
+    private static final String url ="jdbc:postgresql://34.116.236.127:5432/postgres?socketFactory=com.google.cloud.sql.postgres.SocketFactory&cloudSqlInstance=astute-heaven-393613:europe-central2:postgres&user=postgres&password=postgres";
 
 
     public static Connection getConnection() {
